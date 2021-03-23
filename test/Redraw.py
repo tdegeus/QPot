4b99b93fdade9669ae61cafaed670a8d8af5719c
Tom de Geus <tom@geus.me>
2021-03-23 12:04:12 +0100

    Adding Python tests/examples

--- test/Redraw.py
+++ test/Redraw.py
@@ -37,7 +37,7 @@ class Test_main(unittest.TestCase):
             self.assertTrue(np.all(np.equal(y.currentYieldRight(), r)))
             self.assertTrue(np.all(np.equal(y.currentIndex(), i)))
 
-    def test_Reconstruct(self):
+    def test_Reconstruct_positions(self):
 
         seed = int(time.time())
         rand = QPot.random.RandList()
@@ -52,6 +52,9 @@ class Test_main(unittest.TestCase):
             redraw[i] = this.setPosition(i * x)
 
         pos = this.raw_pos()
+        yl = this.currentYieldLeft()
+        yr = this.currentYieldRight()
+        yi = this.currentIndex()
 
         QPot.random.seed(seed)
         other = QPot.RedrawList(x, rand, 30, 5, 2)
@@ -61,6 +64,85 @@ class Test_main(unittest.TestCase):
                 other.setPosition(i * x)
 
         self.assertTrue(np.allclose(pos, other.raw_pos()))
+        self.assertTrue(np.allclose(yl, other.currentYieldLeft()))
+        self.assertTrue(np.allclose(yr, other.currentYieldRight()))
+        self.assertTrue(np.all(np.equal(yi, other.currentIndex())))
+
+    def test_Reconstruct_last_redraw(self):
+
+        seed = int(time.time())
+        rand = QPot.random.RandList()
+
+        x = np.array([4.5, 5.5, 6.5])
+
+        QPot.random.seed(seed)
+        this = QPot.RedrawList(x, rand, 30, 5, 2)
+        redraw = np.zeros((x.size, 50), dtype=np.int64)
+
+        for i in range(redraw.shape[1]):
+            this.setPosition(i * x)
+            redraw[:, i] = this.last_redraw()
+
+        pos = this.raw_pos()
+        yl = this.currentYieldLeft()
+        yr = this.currentYieldRight()
+        yi = this.currentIndex()
+
+        QPot.random.seed(seed)
+        other = QPot.RedrawList(x, rand, 30, 5, 2)
+
+        for i in range(redraw.shape[1]):
+            other.redraw(redraw[:, i])
+
+        other.setPosition(float(redraw.shape[1] - 1) * x)
+
+        self.assertTrue(np.allclose(pos, other.raw_pos()))
+        self.assertTrue(np.allclose(yl, other.currentYieldLeft()))
+        self.assertTrue(np.allclose(yr, other.currentYieldRight()))
+        self.assertTrue(np.all(np.equal(yi, other.currentIndex())))
+
+    def test_Reconstruct_compact_storage(self):
+
+        seed = int(time.time())
+        rand = QPot.random.RandList()
+
+        x = np.array([4.5, 5.5, 6.5])
+
+        QPot.random.seed(seed)
+        this = QPot.RedrawList(x, rand, 30, 5, 2)
+        index = []
+        direction = []
+
+        for i in range(50):
+            this.setPosition(i * x)
+            iredraw = this.last_redraw()
+            if np.any(iredraw > 0):
+                index += [np.argwhere(iredraw > 0).ravel()]
+                direction += [+1]
+            if np.any(iredraw < 0):
+                index += [np.argwhere(iredraw < 0).ravel()]
+                direction += [-1]
+
+        pos = this.raw_pos()
+        yl = this.currentYieldLeft()
+        yr = this.currentYieldRight()
+        yi = this.currentIndex()
+
+        QPot.random.seed(seed)
+        other = QPot.RedrawList(x, rand, 30, 5, 2)
+
+        for i, d in zip(index, direction):
+            if d > 0:
+                other.redrawRight(i)
+            else:
+                other.redrawLeft(i)
+
+        other.setPosition(float(50 - 1) * x)
+
+        self.assertTrue(np.allclose(pos, other.raw_pos()))
+        self.assertTrue(np.allclose(yl, other.currentYieldLeft()))
+        self.assertTrue(np.allclose(yr, other.currentYieldRight()))
+        self.assertTrue(np.all(np.equal(yi, other.currentIndex())))
 
 if __name__ == '__main__':
 
