6068aacbc0d1cc64cd9655487d74f7f80ebe990d
Tom de Geus <tom@geus.me>
2021-08-24 12:04:45 +0200

    Skipping install prefix

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ from setuptools_scm import get_version
 cmake_args = []
 
 if "CMAKE_ARGS" in os.environ:
-    o = list(filter(None, os.environ["CMAKE_ARGS"].split(" ")))
-    if len(o) > 0:
-        cmake_args += o
+    args = list(filter(None, os.environ["CMAKE_ARGS"].split(" ")))
+    if len(args) > 0:
+        cmake_args += [arg for arg in args if len(arg.split("DCMAKE_INSTALL_PREFIX")) == 1]
 
 setup(
     name = 'QPot',
