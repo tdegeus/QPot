d0c7c9efc12c251c056b8d940323fc6d43329694
Tom de Geus <tdegeus@users.noreply.github.com>
2021-08-25 09:49:12 +0200

    [CMake] Minor style updates (#37)

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from skbuild import setup
 from setuptools_scm import get_version
 
+project_name = "QPot"
+
 setup(
-    name = 'QPot',
-    description = 'Library to keep track of a sequential potential energy landscape.',
-    long_description = 'Library to keep track of a sequential potential energy landscape.',
+    name = project_name,
+    description = "Library to keep track of a sequential potential energy landscape.",
+    long_description = "Library to keep track of a sequential potential energy landscape.",
     version = get_version(),
-    license = 'MIT',
-    author = 'Tom de Geus',
-    author_email = 'tom@geus.me',
-    url = 'https://github.com/tdegeus/QPot',
-    packages = ['QPot'],
-    package_dir = {'': 'python'},
-    cmake_install_dir = 'python/QPot',
+    license = "MIT",
+    author = "Tom de Geus",
+    author_email = "tom@geus.me",
+    url = f"https://github.com/tdegeus/{project_name}",
+    packages = [f"{project_name}"],
+    package_dir = {"": "python"},
+    cmake_install_dir = f"python/{project_name}",
     cmake_minimum_required_version = "3.13...3.21",
 )
