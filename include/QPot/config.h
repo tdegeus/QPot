cc2893306276f595af38841b9cd39c1c6c3ff6d7
Tom de Geus <tom@geus.me>
2022-03-23 11:57:51 +0100

    Renaming `boundscheck*` to `inbounds*` to have better user code

--- include/QPot/config.h
+++ include/QPot/config.h
@@ -18,6 +18,10 @@ Macros used in the library.
 #define Q(x) #x
 #define QUOTE(x) Q(x)
 
+#define QPOT_WARNING_IMPL(message, file, line, function) \
+    std::cout << std::string(file) + ":" + std::to_string(line) + " (" + std::string(function) + \
+                     ")" + ": " message ") \n\t";
+
 #define QPOT_ASSERT_IMPL(expr, file, line, function) \
     if (!(expr)) { \
         throw std::runtime_error( \
@@ -76,6 +80,21 @@ Assertions that cannot be disable.
 \throw std::runtime_error
 */
 #define QPOT_REQUIRE(expr) QPOT_ASSERT_IMPL(expr, __FILE__, __LINE__, __FUNCTION__)
+
+/**
+All warnings specific to the Python API are implemented as:
+
+    QPOT_WARNING_PYTHON(...)
+
+They can be enabled by:
+
+    #define QPOT_ENABLE_WARNING_PYTHON
+*/
+#ifdef QPOT_ENABLE_WARNING_PYTHON
+#define QPOT_WARNING_PYTHON(message) QPOT_WARNING_IMPL(message, __FILE__, __LINE__, __FUNCTION__)
+#else
+#define QPOT_WARNING_PYTHON(message)
+#endif
 
 /**
 Keep track of potential energy landscape.
